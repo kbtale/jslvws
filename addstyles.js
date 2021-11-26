722524170ffca7568a2ee710024df2605f213afc
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2021-11-26 14:12:01 -0400

    Update addstyles.js

--- addstyles.js
+++ addstyles.js
@@ -1,1 +1,16 @@
-document.getElementById("Container").style.background = "red";
+var navigationMenu = document.getElementById("P-Navigation");
+var container = document.getElementById("P-Container);
+worksheetsList = document.getElementsByClassName("worksheets");
+selectedWorksheet = 0;
+toggleWorksheets(selectedWorksheet);
+
+for (var i = 0; i < worksheetsList.length; i++) { 
+  menuEl = document.createElement("a");
+  menuEl.value = i
+}
+
+function toggleWorksheets(sw){
+  for (var i = 1; i < worksheetsList.length; i++) { 
+    (i == sw) ? worksheetsList[i].style.display = 'none' : worksheetsList[i].style.display = 'block';
+  }
+}
