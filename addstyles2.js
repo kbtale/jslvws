cc03920556793605f7e0286ef91c7ff62f196534
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2021-11-26 14:27:36 -0400

    Create addstyles2.js

---
+++ addstyles2.js
@@ -0,0 +1,21 @@
+var navigationMenu = document.getElementById("P-Navigation");
+var container = document.getElementById("P-Container");
+worksheetsList = document.getElementsByClassName("liveworksheet");
+selectedWorksheet = 0;
+toggleWorksheets(selectedWorksheet);
+
+for (var i = 0; i < worksheetsList.length; i++) { 
+  menuEl = document.createElement("a");
+  menuEl.value = i;
+  menuEl.style.width = menuEl.style.height = "30px";
+  menuEl.innerHTML = "";
+  menuEl.style.cursor = "pointer";
+  menuEl.style.bakground = "blue";
+  navigationMenu.insertAdjacentElement("beforeend",menuEl);
+}
+
+function toggleWorksheets(sw){
+  for (var i = 1; i < worksheetsList.length; i++) { 
+    (i != sw) ? worksheetsList[i].style.display = 'none' : worksheetsList[i].style.display = 'block';
+  }
+}
