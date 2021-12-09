d0207b4b649333a2c5c6c08e83ee8a2fe541e921
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2021-12-09 03:22:46 -0400

    Update and rename addstyles91.js to addstyles92.js

--- addstyles92.js
+++ addstyles92.js
@@ -221,6 +221,7 @@ function showAll(){
  navigationMenu.style.display = "";
  fnBrowserDetect();
  if (browserName === "chrome") {
+    console.log("Chrome detected");
     for (var j = 0; j < worksheetsList.length; j++){
     worksheetsList[j].getElementsByTagName("div")[0].style.height = "";
     worksheetsList[j].getElementsByTagName("div")[0].getElementsByTagName("div")[0].style.height = "";
