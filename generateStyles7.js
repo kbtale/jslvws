896920239a57157f61da000778fc3378ad9ebeb2
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2022-01-20 02:01:23 -0400

    Update and rename generateStyles6.js to generateStyles7.js

--- generateStyles7.js
+++ generateStyles7.js
@@ -454,7 +454,8 @@ function showAll(){
  if (browserName === "chrome" || browserName === "safari" || browserName === "opera" || browserName === "edge") {
 	show();
    function show(){
-	var worksheetsList = document.getElementsByClassName("liveworksheet");
+	worksheetsList = document.getElementsByClassName("liveworksheet");
+	console.log(JSON.parse(worksheetsList));
 	if (worksheetsList[0].length > 1) {
     for (var j = 0; j < worksheetsList.length; j++){
     worksheetsList[j].getElementsByTagName("div")[0].style.height = "";
